a4c93bcf07c370892264a369190cc2501ae5f510
SHFLEX <139465379+SHFLEX@users.noreply.github.com>
2024-07-20 20:30:36 +0300

    Add files via upload

---
+++ ASCIIAnimation.java
@@ -0,0 +1,218 @@
+public class ASCIIAnimation{
+    public static void main(String[] args) throws InterruptedException{
+        while(true){
+            final int dist = 20;
+            final int sl = 1000;
+            //1
+            System.out.println("L      O        A      D D     I    N   N       G G   ");
+            System.out.println("L     O O      A A     D  D    I    N N N     G       ");
+            System.out.println("L     O O     A A A    D  D    I    N  NN     G  G G  ");
+            System.out.println("L L    O     A     A   D D     I    N   N      G G G  ");
+            System.out.println();
+            System.out.println("||                             ");
+            System.out.println("||                             ");
+            System.out.println("||                             ");
+            System.out.println("||                             ");
+            Thread.sleep(sl);
+
+            for(int i = 0; i<dist; i++){
+                System.out.println();
+            }
+            //2
+            System.out.println("L      O        A      D D     I    N   N       G G   ");
+            System.out.println("L     O O      A A     D  D    I    N N N     G       ");
+            System.out.println("L     O O     A A A    D  D    I    N  NN     G  G G  ");
+            System.out.println("L L    O     A     A   D D     I    N   N      G G G  ");
+            System.out.println();
+            System.out.println("   ||                          ");
+            System.out.println("   ||                          ");
+            System.out.println("   ||                          ");
+            System.out.println("   ||                          ");
+            Thread.sleep(sl);
+
+            for(int i = 0; i<dist; i++){
+                System.out.println();
+            }
+            //3
+            System.out.println("L      O        A      D D     I    N   N       G G   ");
+            System.out.println("L     O O      A A     D  D    I    N N N     G       ");
+            System.out.println("L     O O     A A A    D  D    I    N  NN     G  G G  ");
+            System.out.println("L L    O     A     A   D D     I    N   N      G G G  ");
+            System.out.println();
+            System.out.println("      ||                       ");
+            System.out.println("      ||                       ");
+            System.out.println("      ||                       ");
+            System.out.println("      ||                       ");
+            Thread.sleep(sl);
+
+            for(int i = 0; i<dist; i++){
+                System.out.println();
+            }
+            //4
+            System.out.println("L      O        A      D D     I    N   N       G G   ");
+            System.out.println("L     O O      A A     D  D    I    N N N     G       ");
+            System.out.println("L     O O     A A A    D  D    I    N  NN     G  G G  ");
+            System.out.println("L L    O     A     A   D D     I    N   N      G G G  ");
+            System.out.println();
+            System.out.println("   ||                          ");
+            System.out.println("   ||                          ");
+            System.out.println("   ||                          ");
+            System.out.println("   ||                          ");
+            Thread.sleep(sl);
+
+            for(int i = 0; i<dist; i++){
+                System.out.println();
+            }
+            //5
+            System.out.println("L      O        A      D D     I    N   N       G G   ");
+            System.out.println("L     O O      A A     D  D    I    N N N     G       ");
+            System.out.println("L     O O     A A A    D  D    I    N  NN     G  G G  ");
+            System.out.println("L L    O     A     A   D D     I    N   N      G G G  ");
+            System.out.println();
+            System.out.println("      ||                       ");
+            System.out.println("      ||                       ");
+            System.out.println("      ||                       ");
+            System.out.println("      ||                       ");
+            Thread.sleep(sl);
+
+            for(int i = 0; i<dist; i++){
+                System.out.println();
+            }
+            //6
+            System.out.println("L      O        A      D D     I    N   N       G G   ");
+            System.out.println("L     O O      A A     D  D    I    N N N     G       ");
+            System.out.println("L     O O     A A A    D  D    I    N  NN     G  G G  ");
+            System.out.println("L L    O     A     A   D D     I    N   N      G G G  ");
+            System.out.println();
+            System.out.println("         ||                    ");
+            System.out.println("         ||                    ");
+            System.out.println("         ||                    ");
+            System.out.println("         ||                    ");
+            Thread.sleep(sl);
+
+            for(int i = 0; i<dist; i++){
+                System.out.println();
+            }
+            //7
+            System.out.println("L      O        A      D D     I    N   N       G G   ");
+            System.out.println("L     O O      A A     D  D    I    N N N     G       ");
+            System.out.println("L     O O     A A A    D  D    I    N  NN     G  G G  ");
+            System.out.println("L L    O     A     A   D D     I    N   N      G G G  ");
+            System.out.println();
+            System.out.println("            ||                 ");
+            System.out.println("            ||                 ");
+            System.out.println("            ||                 ");
+            System.out.println("            ||                 ");
+            Thread.sleep(sl);
+
+            for(int i = 0; i<dist; i++){
+                System.out.println();
+            }
+            //8
+            System.out.println("L      O        A      D D     I    N   N       G G   ");
+            System.out.println("L     O O      A A     D  D    I    N N N     G       ");
+            System.out.println("L     O O     A A A    D  D    I    N  NN     G  G G  ");
+            System.out.println("L L    O     A     A   D D     I    N   N      G G G  ");
+            System.out.println();
+            System.out.println("               ||              ");
+            System.out.println("               ||              ");
+            System.out.println("               ||              ");
+            System.out.println("               ||              ");
+            Thread.sleep(sl);
+
+            for(int i = 0; i<dist; i++){
+                System.out.println();
+            }
+            //9
+            System.out.println("L      O        A      D D     I    N   N       G G   ");
+            System.out.println("L     O O      A A     D  D    I    N N N     G       ");
+            System.out.println("L     O O     A A A    D  D    I    N  NN     G  G G  ");
+            System.out.println("L L    O     A     A   D D     I    N   N      G G G  ");
+            System.out.println();
+            System.out.println("                  ||           ");
+            System.out.println("                  ||           ");
+            System.out.println("                  ||           ");
+            System.out.println("                  ||           ");
+            Thread.sleep(sl);
+
+            for(int i = 0; i<dist; i++){
+                System.out.println();
+            }
+            //10
+            System.out.println("L      O        A      D D     I    N   N       G G   ");
+            System.out.println("L     O O      A A     D  D    I    N N N     G       ");
+            System.out.println("L     O O     A A A    D  D    I    N  NN     G  G G  ");
+            System.out.println("L L    O     A     A   D D     I    N   N      G G G  ");
+            System.out.println();
+            System.out.println("                     ||        ");
+            System.out.println("                     ||        ");
+            System.out.println("                     ||        ");
+            System.out.println("                     ||        ");
+            Thread.sleep(sl);
+
+            for(int i = 0; i<dist; i++){
+                System.out.println();
+            }
+            //11
+            System.out.println("L      O        A      D D     I    N   N       G G   ");
+            System.out.println("L     O O      A A     D  D    I    N N N     G       ");
+            System.out.println("L     O O     A A A    D  D    I    N  NN     G  G G  ");
+            System.out.println("L L    O     A     A   D D     I    N   N      G G G  ");
+            System.out.println();
+            System.out.println("                     ||        ");
+            System.out.println("                     ||        ");
+            System.out.println("                     ||        ");
+            System.out.println("                     ||        ");
+            Thread.sleep(sl);
+
+            for(int i = 0; i<dist; i++){
+                System.out.println();
+            }
+            //12
+            System.out.println("L      O        A      D D     I    N   N       G G   ");
+            System.out.println("L     O O      A A     D  D    I    N N N     G       ");
+            System.out.println("L     O O     A A A    D  D    I    N  NN     G  G G  ");
+            System.out.println("L L    O     A     A   D D     I    N   N      G G G  ");
+            System.out.println();
+            System.out.println("                        ||     ");
+            System.out.println("                        ||     ");
+            System.out.println("                        ||     ");
+            System.out.println("                        ||     ");
+            Thread.sleep(sl);
+
+            for(int i = 0; i<dist; i++){
+                System.out.println();
+            }
+            //13
+            System.out.println("L      O        A      D D     I    N   N       G G   ");
+            System.out.println("L     O O      A A     D  D    I    N N N     G       ");
+            System.out.println("L     O O     A A A    D  D    I    N  NN     G  G G  ");
+            System.out.println("L L    O     A     A   D D     I    N   N      G G G  ");
+            System.out.println();
+            System.out.println("                           ||  ");
+            System.out.println("                           ||  ");
+            System.out.println("                           ||  ");
+            System.out.println("                           ||  ");
+            Thread.sleep(sl);
+
+            for(int i = 0; i<dist; i++){
+                System.out.println();
+            }
+            //14
+            System.out.println("L      O        A      D D     I    N   N       G G   ");
+            System.out.println("L     O O      A A     D  D    I    N N N     G       ");
+            System.out.println("L     O O     A A A    D  D    I    N  NN     G  G G  ");
+            System.out.println("L L    O     A     A   D D     I    N   N      G G G  ");
+            System.out.println();
+            System.out.println("                             ||");
+            System.out.println("                             ||");
+            System.out.println("                             ||");
+            System.out.println("                             ||");
+            Thread.sleep(sl);
+
+            for(int i = 0; i<dist; i++){
+                System.out.println();
+            }
+        }
+    }
+}
